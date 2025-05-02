64e65d768355adc39bed0749c464e498e1563c73
mainob <macarena.bustamante@ug.uchile.cl>
2025-05-02 11:44:29 -0400

    Entrega drill 5

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-//Inspala
+//habilita prompt
+//const prompt = require('prompt-sync')() 
 
 //Crea variable prompt
+let r = document.getElementById("result")
+const form = document.getElementById("form")
+
+let randomNum = Math.floor(Math.random() * 10)
+let count = 0
+
+form.addEventListener ("submit", (event) => {
+    event.preventDefault()  
+    let input = document.getElementById("num").value
+
+    if (!isNaN(input)){
+        if(randomNum!=input){
+            r.textContent = "Ups, the secret number is incorrect, try again"
+            count+=1
+        }
+        else {
+            r.textContent = `Congratulations, Secret number is correct!! You failed ${count} times`
+        }
+    }
+    else {
+        alert("Incorrect type value")
+    }
+})
